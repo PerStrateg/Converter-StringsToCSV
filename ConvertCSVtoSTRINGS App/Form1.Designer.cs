9d2c3408bc80d5c4c35048f615e2a72bcd9d279b
PerStrateg <perstrateg@gmail.com>
2017-10-19 12:16:58 +0300

    Initial commit

---
+++ ConvertCSVtoSTRINGS App/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ConvertCSVtoSTRINGS_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Image = global::ConvertCSVtoSTRINGS_App.Properties.Resources.DragAndDrop;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(699, 100);
+            this.label1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(705, 318);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 103);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(699, 185);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 294);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(699, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Конвертировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 318);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Конвертер CSV to STRINGS";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ConvertCSVtoSTRINGS App/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertCSVtoSTRINGS_App
+{
+    public partial class Form1 : Form
+    {
+        DataTable dataTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+
+            dataTable = new DataTable();
+            dataGridView1.DataSource = dataTable;
+            dataTable.Columns.Add("Имя CSV");
+            dataTable.Columns.Add("Имя будущего STRING");
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            foreach (string file in Program.Args)
+                if (CheckIfCSV(file) && !CheckAlreadyExists(file))
+                    dataTable.LoadDataRow((String[])new ArrayList() { file, ConvertCSVtoSTRINGext(file) }.ToArray(typeof(string)), true);
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[]) e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+                if (CheckIfCSV(file) && !CheckAlreadyExists(file))
+                    dataTable.LoadDataRow((String[])new ArrayList() { file, ConvertCSVtoSTRINGext(file) }.ToArray(typeof(string)), true);
+        }
+
+        string ConvertCSVtoSTRINGext(string file)
+        {
+            string a = file;
+
+            a = a.Remove(a.Length - 4, 4);
+            int dotpos = a.LastIndexOf('_');
+            a = a.Remove(dotpos,1);
+            a = a.Insert(dotpos, ".");
+            //a += "STRINGS";
+
+            return a;
+        }
+
+        bool CheckIfCSV(string file)
+        {
+            return (file.Substring(file.Length - 3).Equals("csv") ||
+                file.Substring(file.Length - 3).Equals("Csv") ||
+                file.Substring(file.Length - 3).Equals("CSV"));
+        }
+
+        bool CheckAlreadyExists(string file)
+        {
+            bool flag = false;
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[0].Value.Equals(file))
+                    flag = true;
+            }
+
+            return flag;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String exePath = "StringsPacker.exe";
+
+            ArrayList args = new ArrayList();
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                String line = "";
+                line += dataGridView1.Rows[i].Cells[0].Value + " ";
+                line += dataGridView1.Rows[i].Cells[1].Value + " ";
+                line += "/E1251";
+                args.Add(line);
+            }
+
+            foreach (string line in args)
+            {
+                System.Diagnostics.Process.Start(exePath, line);
+            }
+
+        }
+    }
+}
